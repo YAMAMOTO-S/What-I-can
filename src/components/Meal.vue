3cea5943cf308ea72e74c44220f1bc6926a3e0ed
Shunya <yamasyunizig1216@gmail.com>
2020-02-11 16:10:59 +0900

    Meal DB APIを使って、検索した料理と取得できるようにした。

--- src/components/Meal.vue
+++ src/components/Meal.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
     <v-row align="center" justify="center">
-      <v-col cols="12" md="12" lg="12" xl="12">
+      <v-col cols="12" md="11" lg="10" xl="10">
         <v-card class="card">
           <br />
           <div class="mainimg">
@@ -10,18 +10,28 @@
           <div class="title">
             <h1>Meal Finder</h1>
             <p class="font-weight-light">
-              can find meals by using meal API.
+              Meal name by using meal API.
             </p>
           </div>
 
-          <div class="mainsearch">
-            <v-form>
-              <v-text-field label="enter the meal name"></v-text-field>
-            </v-form>
-            <v-btn class="mainbtn" depressed color="#AAD6EC" rounded>
-              <v-icon color="white">mdi-magnify</v-icon>
-            </v-btn>
+          <div>
+            <form id="submit" class="mainsearch">
+              <v-text-field label="Meal name here" id="search"></v-text-field>
+              <v-btn
+                class="mainbtn"
+                type="submit"
+                depressed
+                color="#AAD6EC"
+                rounded
+              >
+                <v-icon color="white">mdi-magnify</v-icon>
+              </v-btn>
+            </form>
           </div>
+
+          <div class="result"></div>
+          <div id="meals" class="meals"></div>
+          <div id="single-meal"></div>
         </v-card>
       </v-col>
     </v-row>
@@ -29,7 +39,40 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    const search = document.getElementById("search"),
+      submit = document.getElementById("submit"),
+      // mealsEl = document.getElementById('meals'),
+      // result = document.getElementById('result'),
+      single_meal = document.getElementById("single-meal");
+
+    // searchMeal APIから取ってくる
+    function searchMeal(e) {
+      e.preventDefault();
+
+      // singeleMealを消す
+      single_meal.innerHTML = "";
+      // Search Name取得
+      const term = search.value;
+
+      // termに入った名前を検索と空白かどうか確認
+      if(term.trim()){
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+          })
+      } else {
+        alert('Please enter some words.')
+      }
+
+    }
+
+    // イベントリスナー
+    submit.addEventListener("submit", searchMeal);
+  }
+};
 </script>
 
 <style scoped>
@@ -42,17 +85,17 @@ export default {};
   color: #5b5b5b;
 }
 .title p {
-  margin: 20px;
   text-align: center;
   color: #5b5b5b;
 }
 .mainsearch {
+  max-width: 500px;
   justify-content: center;
-  margin: 0 auto;
+  margin: 10px auto;
   display: flex;
 }
-.mainbtn{
+.mainbtn {
   margin-top: 10px;
-  margin-left: 10px; 
+  margin-left: 10px;
 }
 </style>
