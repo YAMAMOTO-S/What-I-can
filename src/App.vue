f3724b2e9fc5d511bf4fbed63086cb2d12ba2e90
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-05-13 18:50:42 +0900

    create about_me

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <v-app>
     <div class="App">
       <div class="header">
-        <h1 class="name">Shunya Yamamoto</h1>
+        <h1 class="name animated fadeIn">Shunya Yamamoto</h1>
       </div>
       <div class="links">
-        <router-link to="/" class="link" exact-active-class="active">自己紹介</router-link>
-        <router-link to="/seatApp" class="link" exact-active-class="active">制作物</router-link>
+        <router-link to="/" class="link">自己紹介</router-link>
+        <router-link to="/seatApp" class="link">制作物</router-link>
       </div>
 
       <main>
@@ -30,23 +30,32 @@ export default {
   height: 440px;
   max-height: 440px;
   background-image: url('https://images.unsplash.com/photo-1469735139965-2d27590468a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3264&q=80');
+  background-size: contain;
   background-size: cover;
   .name {
     text-align: center;
     line-height: 440px;
     font-family: "ヒラギノ丸ゴ ProN",sans-serif;
-    color: #454C50
+    color: white
   }
 }
 .links {
-  font-size: 20px;
+  font-size: 25px;
   text-align: center;
   justify-content: space-between;
+  height: 200px;
+  max-height: 200px;
+  width: 100%;
   .link {
-    color: #454c50;
-    font-weight: bold;
+    color: #6E777C;
     font-family: "ヒラギノ丸ゴ ProN",sans-serif;
-    margin-right: 10px;
+    margin-right: 30px;
+    text-decoration: none;
+    line-height: 200px;
+  }
+  .router-link-exact-active{
+    color: #454C50;
+    border-bottom: #454C50 1px solid;
   }
 }
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,42 @@
 <template>
-   <v-container>
-      <v-row class="animated fadeIn">
-         <h1>自己紹介</h1>
-      </v-row>
-   </v-container>
+   <div class="back">
+      <v-container>
+         <div class="contents animated fadeIn">
+
+            <div class="user_image_desc">
+               <div class="user_image">
+                  <img src="@/assets/web.jpg">
+               </div>
+
+               <div class="user_desc">
+                  <div class="name">
+                     <p>名前: 山本隼也</p>
+                     <br>
+                     <p>年齢: 23</p>
+                     <br>
+                     <p>所在地: 京都</p>
+                     <br>
+                     <p>学歴:</p>
+                     <p class="disc">  2016.04.01 佛教大学 文学部 英米学科入学</p>
+                     <p class="disc">  2020.03.13 佛教大学 文学部 英米学科卒業</p>
+                     <br>
+                     <p>学習歴: </p>
+                     <p class="disc">  2020.02.17 TechExpert(現TechCamp)にてプログラミング学習スタート</p>
+                     <p class="disc">  2020.04.24 TechCamp 卒業</p>
+                     <br>
+                     <p>資格:</p>
+                     <p class="disc">  TOEIC L&Rテスト 780点 </p>
+                  </div>
+               </div>
+            </div>
+               <div class="why">
+                  <p>学習を始めたきっかけ</p>
+               </div>
+
+         </div>
+
+      </v-container>
+   </div>
 </template>
 
 <script>
@@ -16,4 +49,31 @@ export default {
 <style scoped lang="scss">
 @import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css";
 
+.back {
+   background-color: #F5F5F5;
+   height: 800px;
+
+   .user_image_desc {
+      display: flex;
+      margin-top: 40px;
+      .user_image {
+         width: 400px;
+         text-align: center;
+         margin-left: 200px;
+         img {
+            width: 230px;
+            height: 300px;
+            margin-top: 130px;
+         }
+      }
+      .name {
+         font-family: "ヒラギノ丸ゴ ProN",sans-serif;
+         font-size: 16px;
+         margin-left: 100px;
+         .disc {
+            margin-left: 20px;
+         }
+      }
+   }
+}
 </style>
